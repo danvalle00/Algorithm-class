31b106eb2f2c82c343753600b61f467dbf99a7ad
Daniel do Valle <danieldovalle10@gmail.com>
2023-03-10 15:50:07 -0300

    ending exercise

--- array_branches_ex/branches.c
+++ array_branches_ex/branches.c
@@ -9,19 +9,54 @@ A seguir imprima a média vendas para cada filial. */
 
 int main()
 {
-    float sells[branches][months], total_sell[months];
-    int branch;
-    printf("Enter the code for the branch: ");
-    scanf("%d\n", &branch);
+    float sells[branches][months], total_sell[branches], highest_sell_month[branches], lowest_sell_month[branches];
+    int branch[branches];
+    int max = 0;
+    int min = 0;
 
+    // algorithm to input every branch id and every sells for each month
     for (int i = 0; i < branches; i++)
     {
-        printf("Input the sells for the branch: %d", i + 1);
+        printf("Enter the code for the branch %d: ", i + 1);
+        scanf("%d\n", &branch[i]);
+        printf("Input the sells for the branch: %d\n", i + 1);
         for (int j = 0; j < months; j++)
         {
             printf("Month %d: ", j + 1);
-            scanf("%f", &sells[i][j]);
-            total_sell[j] += sells[i][j];
+            scanf("%f\n", &sells[i][j]);
+            total_sell[i] += sells[i][j]; // will sum every month sell for each branch
+            if (sells[i][j] > max)        // condition to seek for the highest and lowest month sell
+            {
+                highest_sell_month[i] = j + 1;
+                max = sells[i][j];
+            }
+            else
+            {
+                lowest_sell_month[i] = j + 1;
+                min = sells[i][j];
+            }
         }
+    }
+    printf("highest sell month for each branch: \n"); // first question
+    for (int i = 0; i < branches; i++)
+    {
+        printf("Branch ID: %d\n", branch[i]);
+        printf("Branch highest sell month: %d\n", highest_sell_month[i]);
+    }
+    printf("\n");
+
+    printf("lowest sell month for each branch: \n"); // second question
+    for (int i = 0; i < branches; i++)
+    {
+        printf("Branch ID: \n", branch[i]);
+        printf("branch lowest sell month: %d\n", lowest_sell_month[i]);
+    }
+    printf("\n");
+
+    printf("average sells for each branch: \n");
+    for (int i = 0; i < branches; i++)
+    {
+        printf("Branch ID: %d\n", branch[i]);
+        printf("branch average sell: %.2f\n", total_sell[i] / (float)months); // espero que num trunque kkkkkkkkkkkkkkk
     }
 }
